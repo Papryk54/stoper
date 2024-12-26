12c3bd10e8dbd6bdcdb18eb89aeff69511ad2efa
Patryk Olszewski <patrykolszewski420@gmail.com>
2024-12-26 13:55:11 +0100

    drugi commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Container from "./Components/Container/Container";
+import TimeDisplay from "./Components/TimeDisplay/TimeDisplay";
+
+function App() {
+	return (
+		<Container>
+			<TimeDisplay></TimeDisplay>
+		</Container>
+	);
+}
+
+export default App;
