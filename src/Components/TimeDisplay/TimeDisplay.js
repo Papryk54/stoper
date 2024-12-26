cf2db02fa2adcfa1c7fd4360f8666728dbf1b4ad
Patryk Olszewski <patrykolszewski420@gmail.com>
2024-12-26 13:29:40 +0100

    pierwszy commit

---
+++ src/Components/TimeDisplay/TimeDisplay.js
@@ -0,0 +1,11 @@
+import styles from "./TimeDisplay.module.scss";
+
+const TimeDisplay = (props) => {
+	return (
+		<div className={styles.TimeDisplay}>
+			<p>Dupa</p>
+		</div>
+	);
+};
+
+export default TimeDisplay;
